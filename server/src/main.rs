1f56d914a8d866faecae50af971f0c3bede7c765
ilya-rb <ilya.ryabchuk@gmail.com>
2025-06-14 20:51:45 +0100

    App migration test

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,15 +1,24 @@
+use anyhow::Context;
 use peek_server::app::App;
+use peek_server::configuration::Settings;
 use peek_server::jobs::cleanup_job::cleanup_old_articles;
 use peek_server::telemetry::LogLevel;
 use peek_server::{configuration, telemetry};
+use sqlx::postgres::PgPoolOptions;
+//use std::env;
 use std::fmt::{Debug, Display};
 use tokio::task::JoinError;
 
 #[tokio::main]
-async fn main() -> Result<(), std::io::Error> {
+async fn main() -> Result<(), anyhow::Error> {
     let settings = configuration::read_configuration().expect("Failed to read app settings");
 
     telemetry::init_tracing(String::from("peek-server"), LogLevel::Info, std::io::stdout);
+
+    // let args: Vec<String> = env::args().collect();
+    // if args.len() > 1 && args[1] == "migrate" {
+    run_migrations(&settings).await?;
+    // }
 
     let worker_settings = settings.clone();
     tokio::spawn(async { cleanup_old_articles(worker_settings).await });
@@ -20,6 +29,17 @@ async fn main() -> Result<(), std::io::Error> {
     tokio::select! {
         outcome = app_worker => report_exit("APP", outcome),
     }
+
+    Ok(())
+}
+
+async fn run_migrations(settings: &Settings) -> Result<(), anyhow::Error> {
+    let db = PgPoolOptions::new()
+        .connect_with(settings.database.connect_options())
+        .await
+        .context("Failed to connect to Postgres")?;
+
+    sqlx::migrate!("./migrations").run(&db).await?;
 
     Ok(())
 }
