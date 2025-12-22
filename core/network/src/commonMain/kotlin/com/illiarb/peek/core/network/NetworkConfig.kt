c202e59254adfd71bd60d94494a096fcf2380a34
ilya-rb <ilya.ryabchuk@gmail.com>
2025-12-22 19:29:57 +0000

    Fixed detekt issues

--- core/network/src/commonMain/kotlin/com/illiarb/peek/core/network/NetworkConfig.kt
+++ core/network/src/commonMain/kotlin/com/illiarb/peek/core/network/NetworkConfig.kt
@@ -3,8 +3,6 @@ package com.illiarb.peek.core.network
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.HttpClientPlugin
 
-private const val DEFAULT_TIMEOUT_SECONDS: Long = 10L
-
 public data class NetworkConfig(
   val timeouts: TimeoutConfig,
 )
@@ -13,7 +11,12 @@ public data class TimeoutConfig(
   val connect: Long = DEFAULT_TIMEOUT_SECONDS,
   val read: Long = DEFAULT_TIMEOUT_SECONDS,
   val write: Long = DEFAULT_TIMEOUT_SECONDS,
-)
+) {
+
+  private companion object {
+    private const val DEFAULT_TIMEOUT_SECONDS: Long = 10L
+  }
+}
 
 internal expect fun createKtorClient(
   config: NetworkConfig,
