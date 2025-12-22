cda6d04840382075139221617d7434bdb15996a4
ilya-rb <ilya.ryabchuk@gmail.com>
2025-12-22 20:29:22 +0000

    Added tests job

--- ui-kit/image-loader/src/iosMain/kotlin/com/illiarb/peek/uikit/imageloader/ImageLoaderPlatformBindings.kt
+++ ui-kit/image-loader/src/iosMain/kotlin/com/illiarb/peek/uikit/imageloader/ImageLoaderPlatformBindings.kt
@@ -1,9 +1,11 @@
 package com.illiarb.peek.uikit.imageloader
 
 import coil3.PlatformContext
+import dev.zacsweers.metro.BindingContainer
 import dev.zacsweers.metro.Provides
 
-public actual interface ImageLoaderPlatformBindings {
+@BindingContainer
+public actual object ImageLoaderPlatformBindings {
 
   @Provides
   public fun providePlatformContext(): PlatformContext = PlatformContext.INSTANCE
