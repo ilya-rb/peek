2a6faf2c2dabbbf420c546a0c2fcdb2733ebe4a7
ilya-rb <ilya.ryabchuk@gmail.com>
2026-01-11 14:19:59 +0000

    Don't switch top bar text when collapsed

--- features/home/src/commonMain/kotlin/com/illiarb/peek/features/home/HomeScreen.kt
+++ features/home/src/commonMain/kotlin/com/illiarb/peek/features/home/HomeScreen.kt
@@ -105,6 +105,8 @@ internal fun HomeScreen(state: HomeScreenContract.State, modifier: Modifier = Mo
         .nestedScroll(bottomBarBehavior.nestedScrollConnection)
         .nestedScroll(topBarBehavior.nestedScrollConnection),
       topBar = {
+        val topBarFullyVisible = topBarBehavior.state.collapsedFraction == 0f
+
         TopAppBar(
           scrollBehavior = topBarBehavior,
           colors = TopAppBarDefaults.topAppBarColors(
@@ -112,7 +114,10 @@ internal fun HomeScreen(state: HomeScreenContract.State, modifier: Modifier = Mo
           ),
           modifier = Modifier.hazeEffect(state = hazeState, style = hazeStyle),
           title = {
-            TopBarTitle(state)
+            TopBarTitle(
+              state = state,
+              textSwitcherEnabled = topBarFullyVisible,
+            )
           },
           actions = {
             TopBarActions(eventSink)
@@ -130,7 +135,10 @@ internal fun HomeScreen(state: HomeScreenContract.State, modifier: Modifier = Mo
 }
 
 @Composable
-private fun TopBarTitle(state: HomeScreenContract.State) {
+private fun TopBarTitle(
+  state: HomeScreenContract.State,
+  textSwitcherEnabled: Boolean,
+) {
   if (state.newsSources.isEmpty()) {
     return
   }
@@ -159,6 +167,7 @@ private fun TopBarTitle(state: HomeScreenContract.State) {
     },
     containerHeightDp = TopAppBarDefaults.TopAppBarExpandedHeight.value.toInt(),
     switchEvery = 5.seconds,
+    enabled = textSwitcherEnabled,
   )
 }
 
